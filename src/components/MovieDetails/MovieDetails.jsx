d16de05e74e0732b62c5f1c33debe12918624f0b
seacat271 <seacat271083@gmail.com>
2022-09-27 00:59:50 +0300

    placeholder

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,6 +1,7 @@
 import { useGetData } from "helpers/useGetData";
 import { Link, useParams, useLocation, Outlet } from "react-router-dom";
 import { DetailsBox, TextTitle, PrimaryBox } from "./MovieDetails.styled";
+import Placeholder from "../../placeholders/placeholder_for_movie.jpg"
 
 
 export const MovieDetails = () => {
@@ -17,7 +18,7 @@ const {poster_path, title, vote_average, overview, genres} = data;
     return  (<PrimaryBox>
         <Link to={location.state?.from ? location.state.from : "/movies"}>Go back</Link>
         <DetailsBox>
-            <img src={"https://image.tmdb.org/t/p/w500/" + poster_path} alt="movie poster" width ="200" height="300" />
+            <img src={"https://image.tmdb.org/t/p/w500/" + (poster_path ? poster_path : Placeholder)} alt="movie poster" width ="200" height="300" />
             <div>
                 <h1>{title}</h1>
                 <p>User score: {Math.round(vote_average *10) + "%"}</p>
