f64b121e5d5a24c0052ba35380d18696680752a2
seacat271 <seacat271083@gmail.com>
2022-09-28 13:10:03 +0300

    repath

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,18 +11,20 @@ const Reviews = lazy (() => import("./Reviews/Reviews"));
 export const App =  () => {
  
   return (
-    <Suspense>
+    <div>
 <Routes>
   <Route path="/" element={<Layout/>}>
-    <Route index element={<Home/>}/>
-    <Route path="movies" element={<Movies/>}/>
-    <Route path="movies/:movieId" element={<MovieDetails/>}>
-      <Route path="cast" element={<Cast/>}/>
-      <Route path="reviews" element={<Reviews/>}/>
-    </Route>
-    <Route path="*" element={<Navigate to="/"/>}></Route>
+    <Suspense>
+      <Route index element={<Home/>}/>
+      <Route path="movies" element={<Movies/>}/>
+      <Route path="movies/:movieId" element={<MovieDetails/>}>
+        <Route path="cast" element={<Cast/>}/>
+        <Route path="reviews" element={<Reviews/>}/>
+      </Route>
+      <Route path="*" element={<Navigate to="/"/>}></Route>
+    </Suspense>
   </Route>
 </Routes>
-</Suspense>
+</div>
   );
 };
